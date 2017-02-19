826d4f9a3c358b7fb45fd61881f61f74ad6ce4f6
trancikk <mrpavlenko@yandex.ru>
2017-02-19 02:00:02 +0200

    Change-Id: I5391aafa2079b3fc000b0cb966ff1ea846430a38 Signed-off-by: trancikk <mrpavlenko@yandex.ru>

--- cool/first/first.py
+++ cool/first/first.py
@@ -1,17 +1,21 @@
 from pybrain.structure import FeedForwardNetwork
 n = FeedForwardNetwork()
 from pybrain.structure import LinearLayer, SigmoidLayer
-inLayer = LinearLayer(2)
-hiddenLayer = SigmoidLayer(3)
-outLayer = LinearLayer(1)
+inLayer = LinearLayer(2, name='in')
+hiddenLayer = SigmoidLayer(3, name='hide')
+outLayer = LinearLayer(1, name='out')
 n.addInputModule(inLayer)
 n.addModule(hiddenLayer)
 n.addOutputModule(outLayer)
 from pybrain.structure import FullConnection
-in_to_hidden = FullConnection(inLayer, hiddenLayer)
-hidden_to_out = FullConnection(hiddenLayer, outLayer)
+in_to_hidden = FullConnection(inLayer, hiddenLayer, name = 'con1')
+hidden_to_out = FullConnection(hiddenLayer, outLayer, name='con2')
 n.addConnection(in_to_hidden)
 n.addConnection(hidden_to_out)
 n.sortModules()
 print n
+n.activate([1, 2])
+print(in_to_hidden.params)
+print(hidden_to_out.params)
+print(n.params)
 print('good')
