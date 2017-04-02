b2132128bf1b47cf2a6e70510ebe4d1573f8956a
Trance <Trance@Trance-pc>
2017-04-02 16:24:59 +0300

    Change-Id: Ie7f491ec35f341d495df4069cfafd07fa111d0fd Signed-off-by: Trance <Trance@Trance-pc>

--- cool/first/person.py
+++ cool/first/person.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import sys
 from test.leakers import test_selftype
+import serial
 reload(sys)
 sys.setdefaultencoding('utf-8')
+data_gens=serial.load('gens_dict')
 class Person(object):
     
 
@@ -10,6 +12,7 @@ class Person(object):
     def __init__(self, args,firstRow):
         self.gens=dict(zip(firstRow,args))
         self.surname=self.gens[u'\u0424\u0418\u041e'].split()
+        
     def printGens(self):
         for i in self.gens:
             print 'Key - %s, value - %s; ' % (i, self.gens[i])
@@ -20,29 +23,38 @@ class Person(object):
     
     def getProp(self):
         return self.gens
+    
     def setProp(self,value,row):
         self.prop[row]=value
      
     def setResult(self,row):
         self.result=row[u'\u0414\u0438\u0430\u0433\u043d\u043e\u0437']
+        
     def getRow(self):
         return self.gens
     
     def setRow(self,row):
-        self.gens_normalise=row
+        self.gens_normalise=list(row)
     
+    def getNormalGen(self):
+        return self.gens_normalise
+        
     def getSurname(self):
-        return self.surname[0]   
+        return self.surname[0]
+       
     def getResult(self):
         try:
             return self.result
         except AttributeError:
             self.result='B'
             return 'В'
+        
     def normaliseGens(self,example):
-        for i in self.gens.keys():
+        for i in sorted(self.gens.keys()):
             if i not in example:
                 self.gens.pop(i)
+        self.setRow([data_gens[i].normalise(i,self.getRow()[i])] for i in sorted(self.gens.keys()))
+                         
     def normaliseResults(self):
         if self.getResult()=='НВ':
             self.result=0
--- cool/first/person_gens_normalise.py
+++ cool/first/person_gens_normalise.py
@@ -5,14 +5,19 @@ import gen
 
 
 data_gens=serial.load('gens_dict')
-data_person=serial.load('fail_norm')
+data_personf=serial.load('fail_norm')
+data_persons=serial.load('success_norm')
+for i in data_personf:
+    print sorted(data_gens.keys())
+    print i.getNormalGen()
+    print i.getRow()
 
-for i in data_person:
-    print sorted(i.getRow().keys())
+#for i in data_person:
+ #   print sorted(i.getRow().keys())
     #for t in i.getRow().keys():
         #print data_gens[t]
         #print i.getRow()[t]
-    i.setRow([data_gens[t].normalise(t,i.getRow()[t]) for t in sorted(i.getRow().keys())])
+  #  i.setRow([data_gens[t].normalise(t,i.getRow()[t]) for t in sorted(i.getRow().keys())])
         #print data_gens[t].normalise(t,i.getRow()[t])
-    print i.gens_normalise
-    print i.printGens()
+   # print i.gens_normalise
+    #print i.printGens()
